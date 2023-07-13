814e5e4cb1276cb13571d3f6c337244fe2c6c996
santoshbilla <sansha508@yahoo.com>
2023-07-13 13:52:00 -0700

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'osse-design-system'`, () => {
+  it(`should have as title 'ose-design-system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('osse-design-system');
+    expect(app.title).toEqual('ose-design-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('osse-design-system app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ose-design-system app is running!');
   });
 });
--- src/app/components/colors/colors.component.ts
+++ src/app/components/colors/colors.component.ts
@@ -6,51 +6,51 @@ import { Component } from "@angular/core";
     styleUrls: ["./colors.component.scss"],
 })
 export class ColorsComponent {
-    osseBlues: any[];
-    osseGreys: any[];
-    osseReds: any[];
+    oseBlues: any[];
+    oseGreys: any[];
+    oseReds: any[];
     backgroundColors: any[];
 
     constructor() {
-        this.osseBlues = [
+        this.oseBlues = [
             {
-                name: "osse-blue-10",
+                name: "ose-blue-10",
                 code: "#00384d",
             },
             {
-                name: "osse-blue-20",
+                name: "ose-blue-20",
                 code: "#004966",
             },
             {
-                name: "osse-blue-30",
+                name: "ose-blue-30",
                 code: "#004d6a",
             },
             {
-                name: "osse-blue-40",
+                name: "ose-blue-40",
                 code: "#006085",
             },
             {
-                name: "osse-blue-50",
+                name: "ose-blue-50",
                 code: "#0092cc",
             },
             {
-                name: "osse-blue-60",
+                name: "ose-blue-60",
                 code: "#00b7ff",
             },
             {
-                name: "osse-blue-70",
+                name: "ose-blue-70",
                 code: "#4dccff",
             },
             {
-                name: "osse-blue-80",
+                name: "ose-blue-80",
                 code: "#8adeff",
             },
             {
-                name: "osse-blue-90",
+                name: "ose-blue-90",
                 code: "#bdecff",
             },
             {
-                name: "osse-blue-100",
+                name: "ose-blue-100",
                 code: "#f0fbff",
             },
         ];
@@ -77,88 +77,88 @@ export class ColorsComponent {
                 code: "#FFFFFF",
             },
         ];
-        this.osseReds = [
+        this.oseReds = [
             {
-                name: "osse-red-10",
+                name: "ose-red-10",
                 code: "#3c0b16",
             },
             {
-                name: "osse-red-20",
+                name: "ose-red-20",
                 code: "#671426",
             },
             {
-                name: "osse-red-30",
+                name: "ose-red-30",
                 code: "#921c35",
             },
             {
-                name: "osse-red-40",
+                name: "ose-red-40",
                 code: "#bc2444",
             },
             {
-                name: "osse-red-50",
+                name: "ose-red-50",
                 code: "#d93a5d",
             },
             {
-                name: "osse-red-60",
+                name: "ose-red-60",
                 code: "#e26580",
             },
             {
-                name: "osse-red-70",
+                name: "ose-red-70",
                 code: "#ea90a3",
             },
             {
-                name: "osse-red-80",
+                name: "ose-red-80",
                 code: "#f2bac6",
             },
             {
-                name: "osse-red-90",
+                name: "ose-red-90",
                 code: "#f7d4dc",
             },
             {
-                name: "osse-red-100",
+                name: "ose-red-100",
                 code: "#fdf2f4",
             },
         ];
 
-        this.osseGreys = [
+        this.oseGreys = [
             {
-                name: "osse-gray-10",
+                name: "ose-gray-10",
                 code: "#262626",
             },
             {
-                name: "osse-gray-20",
+                name: "ose-gray-20",
                 code: "#404040",
             },
             {
-                name: "osse-gray-30",
+                name: "ose-gray-30",
                 code: "#595959",
             },
             {
-                name: "osse-gray-40",
+                name: "ose-gray-40",
                 code: "#737373",
             },
             {
-                name: "osse-gray-50",
+                name: "ose-gray-50",
                 code: "#8c8c8c",
             },
             {
-                name: "osse-gray-60",
+                name: "ose-gray-60",
                 code: "#a6a6a6",
             },
             {
-                name: "osse-gray-70",
+                name: "ose-gray-70",
                 code: "#c4c6c8",
             },
             {
-                name: "osse-gray-80",
+                name: "ose-gray-80",
                 code: "#d9d9d9",
             },
             {
-                name: "osse-gray-90",
+                name: "ose-gray-90",
                 code: "#F2F2F2",
             },
             {
-                name: "osse-gray-100",
+                name: "ose-gray-100",
                 code: "#fafafa",
             },
         ];
